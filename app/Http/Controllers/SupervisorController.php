8d715c50576049b10c9fcf185702162f6a2feff9
BaharHasanova <SW01081496@student.uniten.edu.my>
2024-05-22 15:05:46 +0800

    Add Supervisor Views, Model, Factory, Seeder, Controller

---
+++ app/Http/Controllers/SupervisorController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Volunteer;
+
+class SupervisorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $volunteers = Volunteer::all();
+        return view('supervisor.index', compact('volunteers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('supervisor.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:Volunteers',
+            'password' => 'required|min:6',
+        ]);
+        $validated['password'] = bcrypt($validated['password']);
+        Volunteer::create($validated);
+        return redirect()->route('supervisor.index');
+    
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Volunteer $Volunteer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Volunteer $Volunteer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Volunteer $Volunteer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Volunteer $Volunteer)
+    {
+        //
+    }
+}
