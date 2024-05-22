63de9c9709862a47341eade48c567ae6b19e5ba2
BaharHasanova <SW01081496@student.uniten.edu.my>
2024-05-22 15:40:02 +0800

    Add Member Model, Factory, Seeder, Controller

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Member;
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $members = Member::all();
+        return view('members.index', compact('members'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('members.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Member::create($request->validate([
+            'name' => 'required',
+            'ic_no' => 'required|unique:members',
+            'address' => 'required',
+            'contact_info' => 'required'
+        ]));
+        return redirect()->route('members.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Member $member)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Member $member)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Member $member)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Member $member)
+    {
+        //
+    }
+}
